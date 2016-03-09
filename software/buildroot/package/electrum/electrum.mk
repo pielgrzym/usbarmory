672c3d95b404b7644d6df50ceed077f69790ed13
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2016-03-09 01:07:35 +0100

    added python-six dep

--- software/buildroot/package/electrum/electrum.mk
+++ software/buildroot/package/electrum/electrum.mk
@@ -7,7 +7,7 @@
 ELECTRUM_VERSION = 2.6.2
 ELECTRUM_SOURCE = $(ELECTRUM_VERSION).tar.gz
 ELECTRUM_SITE = https://github.com/spesmilo/electrum/archive
-ELECTRUM_DEPENDENCIES = python python-setuptools python-slowaes python-ecdsa python-requests python-protobuf dnspython python-pbkdf2
+ELECTRUM_DEPENDENCIES = python python-setuptools python-slowaes python-ecdsa python-requests python-protobuf dnspython python-pbkdf2 python-six
 ELECTRUM_SETUP_TYPE = setuptools
 ELECTRUM_LICENSE = MIT
 
